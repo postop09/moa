f49f9304447502c430b73a2ef963336c40b4910e
Hadi <postop09@naver.com>
2026-06-30 16:41:25 +0900

    fix: lint import 수정 및 의존성 추가

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,8 @@
 // https://docs.expo.dev/guides/using-eslint/
-const expoConfig = require('eslint-config-expo/flat');
-const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
-const { defineConfig } = require('eslint/config');
-const { configs } = require('typescript-eslint');
-
+import expoConfig from 'eslint-config-expo/flat';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import { defineConfig } from 'eslint/config';
+import { configs } from 'typescript-eslint';
 
 module.exports = defineConfig([
   expoConfig,
